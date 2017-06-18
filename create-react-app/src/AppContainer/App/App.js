c888b8b969d71e02b885f5ab22f2287b4255e04c
Jonas Jensen <jonas.jensen@greenbird.com>
2017-06-18 16:11:17 +0200

    complete basic conversion functionality

--- create-react-app/src/AppContainer/App/App.js
+++ create-react-app/src/AppContainer/App/App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import {v4} from "uuid";
-import logo from "../../logo.svg";
 import "./App.css";
+import CurrencyDropdown from "./CurrencyDropdown/CurrencyDropdown";
+import CurrencyInput from "./CurrencyInput/CurrencyInput";
 
-const App = ({exchangeAmount, currentRate, convertedExchangeAmount, onInputChange, currencyInfo}) =>
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      <input name="exchangeAmount" type="number" autoFocus={true} value={exchangeAmount} onChange={onInputChange}/>
-      &nbsp;
-      {currencyInfo.base}
-      <br/>
-      <input name="convertedExchangeAmount" type="number" value={convertedExchangeAmount}/>
-      <select name="currentRate" value={currentRate} onChange={onInputChange}>
-        <option value={0} disabled={true}>Select currency</option>
-        {currencyInfo.rates && Object.keys(currencyInfo.rates).map(exchangeRate =>
-          <option value={currencyInfo.rates[exchangeRate]} key={v4()}>{exchangeRate}</option>)}
-      </select>
-    </p>
-  </div>;
+const App = ({
+                 fromExchangeAmount,
+                 fromExchangeRate,
+                 toExchangeAmount,
+                 toExchangeRate,
+                 rates,
+                 fromChange,
+                 toChange,
+                 fromRateChange,
+                 toRateChange,
+             }) =>
+    <div className="App">
+        <p className="App-intro">
+            <CurrencyInput value={fromExchangeAmount} onChange={fromChange} autoFocus />
+            <CurrencyDropdown currentRate={fromExchangeRate} onChange={fromRateChange} rates={rates}/>
+            <br/>
+
+            <CurrencyInput value={toExchangeAmount} onChange={toChange} />
+            <CurrencyDropdown currentRate={toExchangeRate} onChange={toRateChange} rates={rates}/>
+        </p>
+    </div>;
 
 export default App;
--- create-react-app/src/AppContainer/AppContainer.js
+++ create-react-app/src/AppContainer/AppContainer.js
@@ -1,41 +1,80 @@
 import React, {Component} from "react";
+import money from "money";
 import App from "./App/App";
 
 class AppContainer extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      exchangeAmount: 0,
-      currentRate: 0,
-      convertedExchangeAmount: 0,
-      currencyInfo: {},
-    };
+        this.state = {
+            fromExchangeAmount: 0,
+            toExchangeAmount: 0,
 
-    this.changeInput = this.changeInput.bind(this);
-  }
+            fromExchangeRate: "USD",
+            toExchangeRate: "NOK",
 
-  changeInput(event) {
-    this.setState({[event.target.name]: event.target.value}, () =>
-      this.setState({convertedExchangeAmount: (this.state.exchangeAmount * this.state.currentRate).toFixed(2)}));
-  }
+            currencyInfo: {},
+        };
 
-  componentWillMount() {
-    fetch("http://api.fixer.io/latest")
-      .then(res => res.json())
-      .then(res => this.setState({currencyInfo: res}));
-  }
+        this.changeFrom = this.changeFrom.bind(this);
+        this.changeTo = this.changeTo.bind(this);
 
-  render() {
-    return <App
-      exchangeAmount={this.state.exchangeAmount}
-      currentRate={this.state.currentRate}
-      convertedExchangeAmount={this.state.convertedExchangeAmount}
+        this.changeFromCurrencyRate = this.changeFromCurrencyRate.bind(this);
+        this.changeToCurrencyRate = this.changeToCurrencyRate.bind(this);
+    }
 
-      currencyInfo={this.state.currencyInfo}
-      onInputChange={this.changeInput}
-    />;
-  }
+    convert(amount, from, to) {
+        return money.fx(amount).from(from).to(to).toFixed(2);
+    }
+
+    changeFrom(event) {
+        this.setState({fromExchangeAmount: event.target.value}, () => {
+            const {fromExchangeAmount, fromExchangeRate, toExchangeRate} = this.state;
+            this.setState({toExchangeAmount: this.convert(fromExchangeAmount, fromExchangeRate, toExchangeRate)});
+        });
+    }
+
+    changeTo(event) {
+        this.setState({toExchangeAmount: event.target.value}, () => {
+            const {toExchangeAmount, fromExchangeRate, toExchangeRate} = this.state;
+            this.setState({fromExchangeAmount: this.convert(toExchangeAmount, toExchangeRate, fromExchangeRate)});
+        });
+    }
+
+    changeFromCurrencyRate(event) {
+        this.setState({fromExchangeRate: event.target.value}, () => {
+            const {toExchangeAmount, fromExchangeRate, toExchangeRate} = this.state;
+            this.setState({fromExchangeAmount: this.convert(toExchangeAmount, fromExchangeRate, toExchangeRate)});
+        });
+    }
+
+    changeToCurrencyRate(event) {
+        this.setState({toExchangeRate: event.target.value}, () => {
+            const {fromExchangeAmount, fromExchangeRate, toExchangeRate} = this.state;
+            this.setState({toExchangeAmount: this.convert(fromExchangeAmount, fromExchangeRate, toExchangeRate)});
+        });
+    }
+
+    componentWillMount() {
+        fetch("http://api.fixer.io/latest")
+            .then(res => res.json())
+            .then(res => {
+                this.setState({rates: res.rates});
+                money.rates = res.rates;
+            });
+    }
+
+    render() {
+        return (
+            <App
+                {...this.state}
+                fromChange={this.changeFrom}
+                toChange={this.changeTo}
+                fromRateChange={this.changeFromCurrencyRate}
+                toRateChange={this.changeToCurrencyRate}
+            />
+        );
+    }
 }
 
 export default AppContainer;
